aeb80123c56cba55125bab1218f9737bbeb5f054
edgar965 <e@edgarm.de>
2026-05-31 00:37:09 +0200

    Version bump auf 0.50

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 bl_info = {
     "name": "HumanBody",
     "author": "Edgar",
-    "version": (0, 0, 49),
+    "version": (0, 0, 50),
     "blender": (5, 0, 0),
     "location": "View3D > Sidebar > HumanBody",
     "description": "Parametric Human Body with animation and film creation",
